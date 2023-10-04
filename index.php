7229ec8b310c58d7612fd442e645b176368c8f7a
Luskans <sylvain.migne@gmail.com>
2023-10-04 13:35:36 +0200

    some little change on index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include_once('./partials/header.php') ?>
 
-<body class="index">
+<!-- <body class="index">
   <main class="container d-flex flex-column justify-content-center align-items-center">
     <h1 class="index-title">L'infini dans les cieux ! </h1>
     <form method="post" action="./process/add_user.php">
@@ -9,6 +9,16 @@
           <button class="form-control btn btn-secondary" type="submit">Se connecter</button>
       </div>
   </div>
+  </form>
+</body> -->
+
+<body class="index d-flex flex-column justify-content-center align-items-center">
+  <h1 class="index-title">L'infini dans les cieux ! </h1>
+  <form method="post" action="./process/add_user.php">
+    <div class="mt-5 input-group mb-3">
+      <input class="form-control" type="text" name="name" placeholder="Entrez un pseudo...">
+      <button class="form-control btn btn-secondary" type="submit">Se connecter</button>
+    </div>
   </form>
 </body>
 
